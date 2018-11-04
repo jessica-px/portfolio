bbc95bac1ec0b2d80a9f4d009afaa49fd62c227f
Jessica Peck <jessypeck@gmail.com>
2018-11-04 13:41:24 -0800

    Init

---
+++ script.ts
@@ -0,0 +1,103 @@
+const onLoad = () => {
+  getProjectData();
+}
+
+const getProjectData = () => {
+  fetch('./projects.json').then(response => {
+    return response.json();
+  }).then(data => {
+      buildProjectCards(data.projectsArray);
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+interface Project{
+  name: string,
+  image: string
+  content: string[],
+  siteLink: string,
+  codeLink: string,
+  skills: string[]
+}
+
+const buildProjectCards = (projectData: Project[]) => {
+  for (let project of projectData){
+    const newCard = createCard();
+    getImage(newCard, project);
+    getContent(newCard, project);
+    document.getElementById("projects-container").appendChild(newCard);
+  }
+}
+
+const createCard = (): HTMLDivElement => {
+  const newCard = document.createElement("div");
+  newCard.className = 'project-card'
+  return newCard;
+}
+
+const getImage = (card: HTMLDivElement, project: Project) => {
+  const newImage = document.createElement("img");
+  newImage.src = project.image;
+  newImage.className = 'card-image';
+  card.appendChild(newImage);
+}
+
+const getContent = (card: HTMLDivElement, project: Project) => {
+  const content = document.createElement("div");
+  content.className = 'card-content';
+  getTitle(content, project);
+  getBodyContent(content, project);
+  getSkillChips(content, project);
+  card.appendChild(content);
+}
+
+const getTitle = (div: HTMLDivElement, project: Project) => {
+  const titleRow = document.createElement("div");
+  titleRow.className = 'card-title-row';
+  const newTitle = document.createElement("div");
+  newTitle.className = 'card-title';
+  newTitle.innerHTML += project.name;
+  titleRow.appendChild(newTitle);
+  getButtons(titleRow, project);
+  div.appendChild(titleRow);
+
+}
+
+const getButtons = (div: HTMLDivElement, project: Project) => {
+  // View Button
+  const viewButton = document.createElement("a");
+  viewButton.className = 'card-button visit';
+  viewButton.href = project.siteLink;
+  viewButton.innerHTML = 'Visit Site <i class="fas fa-external-link-alt"></i>';
+  // Code Button
+  const codeButton = document.createElement("a");
+  codeButton.className = 'code';
+  codeButton.href = project.codeLink;
+  codeButton.innerHTML = 'View Code <i class="far fa-code"></i>';
+  // Wrapper
+  const buttonWrapper = document.createElement("div");
+  buttonWrapper.className = 'card-button-wrapper';
+  buttonWrapper.appendChild(viewButton);
+  buttonWrapper.appendChild(codeButton);
+  div.appendChild(buttonWrapper);
+}
+
+const getBodyContent = (div: HTMLDivElement, project: Project) => {
+  div.innerHTML += project.content.join('');
+}
+
+const getSkillChips = (div: HTMLDivElement, project: Project) => {
+  const chipWrapper = document.createElement("div");
+  chipWrapper.className = 'chip-wrapper';
+  for (let skill of project.skills){
+    const chip = document.createElement("span");
+    chip.className = 'card-chip';
+    chip.innerHTML = skill;
+    chipWrapper.appendChild(chip);
+  }
+  div.appendChild(chipWrapper);
+}
+
+onLoad();
+
